752933dc56581063ef6563a273ba86265b76f245
Ayomide <adeshinaayomide12@yahoo.com>
2023-10-17 13:16:13 +0100

    Add: Template Files

---
+++ main.py
@@ -0,0 +1,14 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+  return "<h1>Welcome to PipeOps</h1>"
+
+if __name__ == '__main__':
+  # Use PORT environment variable if available, or default to 5000
+  port = int(os.environ.get('PORT', 5000))
+  app.run(host='0.0.0.0', port=port)
+
